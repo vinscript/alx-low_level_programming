cf0da0d04125c23704a1b03b05bcab243d317475
vinscript <vinndubuisi39@gmail.com>
2023-02-16 14:19:38 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
  * main - A C program that prints a line to the standard error
  * Return 1 (Success)
  */
-
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
