1910682349c5ba5a90ff74aab9f01ef6f00d05cc
vinscript <vinndubuisi39@gmail.com>
2023-02-17 14:58:45 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower-case
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
