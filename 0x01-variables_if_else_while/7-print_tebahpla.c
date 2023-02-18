d35470b7a7d6e7252fcb815cafb309524b5f2bb2
vinscript <vinndubuisi39@gmail.com>
2023-02-17 20:57:52 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that prints the lowercase alphabet in reverse
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int low;
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
