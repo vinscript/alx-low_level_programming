439ac271490c81f768f82d7dae1765577406504a
vinscript <vinndubuisi39@gmail.com>
2023-02-17 20:33:13 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints all single digit numbers of base 10 from 0
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+
+}
