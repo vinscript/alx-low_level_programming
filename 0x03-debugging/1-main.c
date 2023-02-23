fb18117d5987642f3d7cb87b92ba2f3279d52ec1
vinscript <vinndubuisi39@gmail.com>
2023-02-23 12:57:46 -0800

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Causes an infinite loop
+ * Return : 0
+ */
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/*
+ * while (i < 0)
+ * {
+ * 		putchar(i);
+ *}
+ */
+printf("Infinite loop avoided! \\0/\n");
+
+return (0);
+}
