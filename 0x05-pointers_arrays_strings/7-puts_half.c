d945c30044acacadbfd45851dc14f407533838f0
vinscript <vinndubuisi39@gmail.com>
2023-02-27 19:51:49 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: inut
+ */
+void puts_half(char *str)
+{
+	int len = 0
+
+		while (*str != '\0')
+		{
+			len++;
+			str++;
+		}
+
+		str -= (len / 2);
+		while (*str != '\0')
+		{
+			_puchar(*str);
+			str++;
+		}
+
+		_putchar('\n');
+
+
