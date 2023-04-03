f586067aaea3be010c0d3e64dc96a896741cc386
vinscript <vinndubuisi39@gmail.com>
2023-04-03 15:34:52 -0700

    2. Add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new note at the begining of a linked list
+ * @head: pointer to the first nofde in the list
+ * @n: data to insert the new code
+ *
+ * Return: pointer to the new code, or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n);
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+		*head = new;
+
+		return (new);
+
+}
